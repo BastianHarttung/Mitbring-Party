29a1dedf23cce55007013a1f739e5c9c210d270a
BastianHarttung <info@harttung-fotografie.de>
2022-01-29 16:05:21 +0100

    move test partycollection to /mockup

--- src/App.js
+++ src/App.js
@@ -6,42 +6,11 @@ import Header from "./components/Header";
 import Wahl from "./pages/Wahl";
 import Start from "./pages/Start";
 import Uebersicht from "./pages/Uebersicht";
-import {Essen, Party} from "./models/party";
+
+import {testPartyCollection} from "./mockup/testConstants";
 
 
 function App() {
-
-    const emptyParty = new Party('', '', '', '', [])
-    const testPartyCollection = [
-        new Party('Fondue 2021', 'Hip', '2021-12-25', 'Das alljährliche Fondue.',
-            [
-                new Essen('Salate', 'Kartoffelsalat', 'Bastian'),
-                new Essen('Salate', 'Tomatensalat', 'Sabrina'),
-                new Essen('Sonstiges', 'Würstchen', 'Sabrina'),
-                new Essen('Süßes', 'Kuchen', ''),
-                new Essen('Süßes', 'Schokolade', '')
-            ]),
-        new Party('Picknick Geburtstagsparty Basti 2022', 'Rothsee', '2022-05-23', 'Das wird ein Geburtstagspicknick',
-            [
-                new Essen('Salate', 'Blattsalat', 'Bastian'),
-                new Essen('Salate', 'Nudelsalat', 'Sabrina'),
-                new Essen('Sonstiges', 'Eier', 'Patrick'),
-                new Essen('Süßes', 'Kuchen', 'Silke'),
-                new Essen('Süßes', 'Muffin', ''),
-                new Essen('Süßes', 'Gummibären', ''),
-            ])
-    ]
-    const testParty = new Party(
-        'Fondue 2021',
-        'Hip',
-        '23.5.2022',
-        'Das wird ein Geburtstagspicknick',
-        [
-            new Essen('Salate', 'Kartoffelsalat', 'Bastian'),
-            new Essen('Salate', 'Nudelsalat', 'Sabrina'),
-            new Essen('Sonstiges', 'Würstchen', ''),
-            new Essen('Süßes', 'Kuchen', '')
-        ])
 
     const [partyCollection, setPartyCollection] = useState(testPartyCollection)
 
---
+++ src/mockup/testConstants.js
@@ -0,0 +1,35 @@
+import {Essen, Party} from "../models/party";
+
+
+export const emptyParty = new Party('', '', '', '', [])
+export const testPartyCollection = [
+    new Party('Fondue 2021', 'Hip', '2021-12-25', 'Das alljährliche Fondue.',
+        [
+            new Essen('Salate', 'Kartoffelsalat', 'Bastian'),
+            new Essen('Salate', 'Tomatensalat', 'Sabrina'),
+            new Essen('Sonstiges', 'Würstchen', 'Sabrina'),
+            new Essen('Süßes', 'Kuchen', ''),
+            new Essen('Süßes', 'Schokolade', '')
+        ]),
+    new Party('Picknick Geburtstagsparty Basti 2022', 'Rothsee', '2022-05-23', 'Das wird ein Geburtstagspicknick',
+        [
+            new Essen('Salate', 'Blattsalat', 'Bastian'),
+            new Essen('Salate', 'Nudelsalat', 'Sabrina'),
+            new Essen('Sonstiges', 'Eier', 'Patrick'),
+            new Essen('Süßes', 'Kuchen', 'Silke'),
+            new Essen('Süßes', 'Muffin', ''),
+            new Essen('Süßes', 'Gummibären', ''),
+        ])
+]
+
+export const testParty = new Party(
+    'Fondue 2021',
+    'Hip',
+    '23.5.2022',
+    'Das wird ein Geburtstagspicknick',
+    [
+        new Essen('Salate', 'Kartoffelsalat', 'Bastian'),
+        new Essen('Salate', 'Nudelsalat', 'Sabrina'),
+        new Essen('Sonstiges', 'Würstchen', ''),
+        new Essen('Süßes', 'Kuchen', '')
+    ])
