7dc77a9d51e52ff1d7ab4df9889797294278e41f
BastianHarttung <info@harttung-fotografie.de>
2023-05-20 23:16:10 +0200

    feat(Hilfe): short text to explain that section is in Progress

--- src/pages/Hilfe.tsx
+++ src/pages/Hilfe.tsx
@@ -2,6 +2,9 @@ const Hilfe = () => {
   return (
     <section>
       <h3>Hilfe</h3>
+      <p>Diese Sektion ist noch in Arbeit. Wenn ihr Hilfe bei der Benutzung braucht meldet euch bei mir :)</p>
+      <hr style={{width:"100%"}}/>
+      <p>© by Bastian Harttung</p>
     </section>
   );
 };
