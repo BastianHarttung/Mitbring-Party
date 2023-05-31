220d25d97e3214a40363aa1678c131933edf8af4
BastianHarttung <info@harttung-fotografie.de>
2023-05-31 22:29:06 +0200

    feat(foodRow): add kategorie to food input

--- src/components/admin/foodRow.tsx
+++ src/components/admin/foodRow.tsx
@@ -16,12 +16,21 @@ const FoodRow = ({essen, index, onChange, onDelete}: FoodRowProps) => {
   return (
     <div className={classes.essenContainer}>
       <input type="text"
+             className={classes.essenName}
              value={essen.essenName}
              onChange={(event) => onChange(event, index, "essenName")}/>
-      <input type="text"
-             value={essen.werBringts}
-             onChange={(event) => onChange(event, index, "werBringts")}/>
-      <MdDelete onClick={() => onDelete(essen)}/>
+
+      <div className={classes.input_second_row}>
+        <input type="text"
+               className={classes.kategorie}
+               value={essen.kategorie}
+               onChange={(event) => onChange(event, index, "kategorie")}/>
+        <input type="text"
+               className={classes.werBringts}
+               value={essen.werBringts}
+               onChange={(event) => onChange(event, index, "werBringts")}/>
+        <MdDelete onClick={() => onDelete(essen)}/>
+      </div>
     </div>
   );
 };
